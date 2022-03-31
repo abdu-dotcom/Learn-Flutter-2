cf371eeade76fb832bebe42c93afa88a204fe2e7
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-03-31 17:26:42 +0700

    ListView.Builder

--- lib/main.dart
+++ lib/main.dart
@@ -27,23 +27,23 @@ class ScrollingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: numberList.map((number) {
-          return Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
+        body: ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            border: Border.all(color: Colors.black),
+          ),
+          child: Center(
+            child: Text(
+              '${numberList[index]}',
+              style: TextStyle(fontSize: 50),
             ),
-            child: Center(
-              child: Text(
-                '$number',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          );
-        }).toList(),
-      ),
-    );
+          ),
+        );
+      },
+      itemCount: numberList.length,
+    ));
   }
 }
